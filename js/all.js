325e8cf9d823fd2761553cee0f27220e54bfc6af
roceil <a0978006326@gmail.com>
2022-06-02 22:27:54 +0800

    增加方案費用告警

--- js/all.js
+++ js/all.js
@@ -3,5 +3,12 @@ $(document).ready(function () {
         $('html,body').animate({ scrollTop: 0 }, 'slow');   /* 返回到最頂上 */
         return false;
     });
-    
+    $(`.home-btn`).click(function (e) { 
+        e.preventDefault();
+        alert(`想幹嘛？有種說出來`)
+    });
+    $(`.footer-btn`).click(function (e) { 
+        e.preventDefault();
+        alert(`你以為會有好東西嗎？`)
+    });
 });
