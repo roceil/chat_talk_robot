eee4c6f8292215fb518e4732878cf9c9eb6f8559
roceil <a0978006326@gmail.com>
2022-06-03 20:02:13 +0800

    chatTalkerV2.0

--- js/all.js
+++ js/all.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $(`.btn-topball`).click(function (e) {
+    $(`.topball`).click(function (e) {
         $('html,body').animate({ scrollTop: 0 }, 'slow');   /* 返回到最頂上 */
         return false;
     });
